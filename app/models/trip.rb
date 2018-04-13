1101a63dcabb37cb275be6c1e88c14891937f1ef
Evan Wheeler <anon0mys87@gmail.com>
2018-04-13 10:57:32 -0600

    Trail metrics on trip show page

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,4 +4,16 @@ class Trip < ApplicationRecord
   def planned_distance
     trails.sum(:length)
   end
+
+  def average_distance
+    trails.average(:length)
+  end
+
+  def longest_distance
+    trails.maximum(:length)
+  end
+
+  def shortest_distance
+    trails.minimum(:length)
+  end
 end
--- spec/features/trips/user_visits_trip_show_spec.rb
+++ spec/features/trips/user_visits_trip_show_spec.rb
@@ -37,5 +37,38 @@ describe 'Visits a trip show' do
 
       expect(page).to have_content('Planned Distance: 21')
     end
+
+    scenario 'they see average hiking distance for that trip' do
+      trip = Trip.create(name: 'Trip 1', start_date: '04/11/2018', end_date: '04/11/2018')
+      trail_one = trip.trails.create(name: 'Trail 1', length: 6, address: '123 Nowhere Road')
+      trail_two = trip.trails.create(name: 'Trail 2', length: 7, address: '1234 Nowhere Road')
+      trail_three = trip.trails.create(name: 'Trail 3', length: 8, address: '12345 Nowhere Road')
+
+      visit trip_path(trip)
+
+      expect(page).to have_content('Average Distance: 7')
+    end
+
+    scenario 'they see longest hiking distance for that trip' do
+      trip = Trip.create(name: 'Trip 1', start_date: '04/11/2018', end_date: '04/11/2018')
+      trail_one = trip.trails.create(name: 'Trail 1', length: 6, address: '123 Nowhere Road')
+      trail_two = trip.trails.create(name: 'Trail 2', length: 7, address: '1234 Nowhere Road')
+      trail_three = trip.trails.create(name: 'Trail 3', length: 8, address: '12345 Nowhere Road')
+
+      visit trip_path(trip)
+
+      expect(page).to have_content('Longest Distance: 8')
+    end
+
+    scenario 'they see shortest hiking distance for that trip' do
+      trip = Trip.create(name: 'Trip 1', start_date: '04/11/2018', end_date: '04/11/2018')
+      trail_one = trip.trails.create(name: 'Trail 1', length: 6, address: '123 Nowhere Road')
+      trail_two = trip.trails.create(name: 'Trail 2', length: 7, address: '1234 Nowhere Road')
+      trail_three = trip.trails.create(name: 'Trail 3', length: 8, address: '12345 Nowhere Road')
+
+      visit trip_path(trip)
+
+      expect(page).to have_content('Shortest Distance: 6')
+    end
   end
 end
